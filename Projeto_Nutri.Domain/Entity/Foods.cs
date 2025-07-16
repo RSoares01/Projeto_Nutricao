8337b638fabba85196455f5ddba3121ca83bed37
unknown <rafaelsoares16032001@gmail.com>
2025-07-16 19:16:01 -0300

    Commit Inicial

---
+++ Projeto_Nutri.Domain/Entity/Foods.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Projeto_Nutri.Domain.Entity
+{
+    public class Foods
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        
+        public decimal Caloriaspor100g {  get; set; }
+    }
+}
